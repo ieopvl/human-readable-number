bc8661f5d5e73b7971e5ce8ab8d8d1ec1b5560ca
Pavel Ivchenko <pavel.ivchenko.js@mail.ru>
2020-10-08 21:44:56 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function toReadable (number) {
-  
+    if (number === 0) return 'zero';
+
+    let result = '';
+    const digits = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+    const dozens = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+
+    const numberString = String(number);
+
+    const condition1 = numberString.length === 1 || numberString[0] === '1' && numberString.length === 2;
+    const condition2 = numberString.length === 2 && numberString[0] !== '1';
+    const condition3 = numberString.length === 3;
+
+    if (condition1) result = digits[number];
+    if (condition2) result = dozens[numberString[0]] + ' ' + toReadable(numberString[1]);
+    if (condition3) result = digits[numberString[0]] + 'hundred ' + toReadable(numberString.slice(1));
+
+    return result.trim();
 }
